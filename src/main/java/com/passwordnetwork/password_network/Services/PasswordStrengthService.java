ed0c51523228c1515e20faefd80754a119e0d8a8
Pedro22D0W <pedro.rodolfo.galvao22@hotmail.com>
2025-08-16 19:59:04 -0300

    apiURl

--- src/main/java/com/passwordnetwork/password_network/Services/PasswordStrengthService.java
+++ src/main/java/com/passwordnetwork/password_network/Services/PasswordStrengthService.java
@@ -1,6 +1,8 @@
 package com.passwordnetwork.password_network.Services;
 
 import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
@@ -8,7 +10,8 @@ import com.passwordnetwork.password_network.DTO.StrengthenPasswordDTO;
 
 @Service
 public class PasswordStrengthService {
-
+    @Value("${strong.password.api.url}")
+    private String apiUrl;
     private final WebClient webClient;
 
     public PasswordStrengthService(WebClient webClient) {
@@ -16,8 +19,8 @@ public class PasswordStrengthService {
     }
 
     public String strengthenPassword(String weakPassword) {
-        // Substitua a URL pela da API real
-        String apiUrl = "http://localhost:8081/strongme";
+       
+        
         StrengthenPasswordDTO apiDTO = new StrengthenPasswordDTO(weakPassword,12,true,true,true);
 
         return webClient.post()
